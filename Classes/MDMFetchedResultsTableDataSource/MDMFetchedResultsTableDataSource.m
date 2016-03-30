5340fc2849b9856fbe3adc4d1d3d0e6bf55747a6
Udo Hoppenworth <u.hoppenworth@gmail.com>
2016-03-30 03:32:18 -0400

    Fix tableView updates when inserting sections with rows

--- Classes/MDMFetchedResultsTableDataSource/MDMFetchedResultsTableDataSource.m
+++ Classes/MDMFetchedResultsTableDataSource/MDMFetchedResultsTableDataSource.m
@@ -28,6 +28,12 @@
 
 @property (nonatomic, weak) UITableView *tableView;
 
+@property (nonatomic, strong) NSMutableIndexSet *deletedSectionIndexes;
+@property (nonatomic, strong) NSMutableIndexSet *insertedSectionIndexes;
+@property (nonatomic, strong) NSMutableArray *deletedRowIndexPaths;
+@property (nonatomic, strong) NSMutableArray *insertedRowIndexPaths;
+@property (nonatomic, strong) NSMutableArray *updatedRowIndexPaths;
+
 @end
 
 @implementation MDMFetchedResultsTableDataSource
@@ -179,81 +185,123 @@ forRowAtIndexPath:(NSIndexPath *)indexPath {
 
 #pragma mark - NSFetchedResultsControllerDelegate
 
+// reference: https://gist.github.com/MrRooni/4988922
+// http://www.fruitstandsoftware.com/blog/2013/02/19/uitableview-and-nsfetchedresultscontroller-updates-done-right/
+
 - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
     
-    [self.tableView endUpdates];
-}
-
-- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
-    [self.tableView beginUpdates];
-}
-
-- (void)controller:(NSFetchedResultsController *)controller
-  didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
-           atIndex:(NSUInteger)sectionIndex
-     forChangeType:(NSFetchedResultsChangeType)type {
-   
-    switch (type) {
-        case NSFetchedResultsChangeInsert:
-            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex]
-                          withRowAnimation:UITableViewRowAnimationAutomatic];
-            
-            break;
-
-        case NSFetchedResultsChangeDelete:
-            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex]
-                          withRowAnimation:UITableViewRowAnimationAutomatic];
-            
-            break;
-            
-        default:
-            ALog(@"Missing NSFechedResultsChange case");
-            
-            break;
+    NSInteger totalChanges = [self.deletedSectionIndexes count] +
+    [self.insertedSectionIndexes count] +
+    [self.deletedRowIndexPaths count] +
+    [self.insertedRowIndexPaths count] +
+    [self.updatedRowIndexPaths count];
+    if (totalChanges > 50) {
+        [self.tableView reloadData];
+        return;
     }
+    
+    [self.tableView beginUpdates];
+    
+    [self.tableView deleteSections:self.deletedSectionIndexes withRowAnimation:UITableViewRowAnimationAutomatic];
+    [self.tableView insertSections:self.insertedSectionIndexes withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+    [self.tableView deleteRowsAtIndexPaths:self.deletedRowIndexPaths withRowAnimation:UITableViewRowAnimationLeft];
+    [self.tableView insertRowsAtIndexPaths:self.insertedRowIndexPaths withRowAnimation:UITableViewRowAnimationRight];
+    [self.tableView reloadRowsAtIndexPaths:self.updatedRowIndexPaths withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+    [self.tableView endUpdates];
+    
+    // nil out the collections so their ready for their next use.
+    self.insertedSectionIndexes = nil;
+    self.deletedSectionIndexes = nil;
+    self.deletedRowIndexPaths = nil;
+    self.insertedRowIndexPaths = nil;
+    self.updatedRowIndexPaths = nil;
 }
 
-- (void)controller:(NSFetchedResultsController *)controller
-   didChangeObject:(id)anObject
-       atIndexPath:(NSIndexPath *)indexPath
-     forChangeType:(NSFetchedResultsChangeType)type
-      newIndexPath:(NSIndexPath *)newIndexPath {
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id )sectionInfo atIndex:(NSUInteger)sectionIndex
+     forChangeType:(NSFetchedResultsChangeType)type {
     
     switch (type) {
         case NSFetchedResultsChangeInsert:
-            [self.tableView insertRowsAtIndexPaths:@[newIndexPath]
-                                  withRowAnimation:UITableViewRowAnimationAutomatic];
-           
+            [self.insertedSectionIndexes addIndex:sectionIndex];
             break;
-
         case NSFetchedResultsChangeDelete:
-            [self.tableView deleteRowsAtIndexPaths:@[indexPath]
-                                  withRowAnimation:UITableViewRowAnimationAutomatic];
-           
-            break;
-
-        case NSFetchedResultsChangeUpdate:
-            if ([self.tableView.indexPathsForVisibleRows containsObject:indexPath]) {
-                
-                [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-            }
-           
-            break;
-
-        case NSFetchedResultsChangeMove:
-            // It's currently not possible to perform a move and reload inside the same tableview update block (http://www.hitmaroc.net/1166896-9192-how-reload-programmatically-moved-row.html).
-            // The workaround is to delete then insert the row.
-            [self.tableView deleteRowsAtIndexPaths:@[indexPath]
-                                  withRowAnimation:UITableViewRowAnimationFade];
-            [self.tableView insertRowsAtIndexPaths:@[newIndexPath]
-                                  withRowAnimation:UITableViewRowAnimationAutomatic];
-
+            [self.deletedSectionIndexes addIndex:sectionIndex];
             break;
         default:
-            ALog(@"Missing NSFechedResultsChange case");
-
+            ; // Shouldn't have a default
             break;
     }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath
+     forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
+    
+    if (type == NSFetchedResultsChangeInsert) {
+        if ([self.insertedSectionIndexes containsIndex:newIndexPath.section]) {
+            // If we've already been told that we're adding a section for this inserted row we skip it since it will handled by the section insertion.
+            return;
+        }
+        [self.insertedRowIndexPaths addObject:newIndexPath];
+    } else if (type == NSFetchedResultsChangeDelete) {
+        if ([self.deletedSectionIndexes containsIndex:indexPath.section]) {
+            // If we've already been told that we're deleting a section for this deleted row we skip it since it will handled by the section deletion.
+            return;
+        }
+        [self.deletedRowIndexPaths addObject:indexPath];
+    } else if (type == NSFetchedResultsChangeMove) {
+        if ([self.insertedSectionIndexes containsIndex:newIndexPath.section] == NO) {
+            [self.insertedRowIndexPaths addObject:newIndexPath];
+        }
+        if ([self.deletedSectionIndexes containsIndex:indexPath.section] == NO) {
+            [self.deletedRowIndexPaths addObject:indexPath];
+        }
+    } else if (type == NSFetchedResultsChangeUpdate) {
+        [self.updatedRowIndexPaths addObject:indexPath];
+    }
+}
+
+
+#pragma mark - Overridden getters
+
+/**
+ * Lazily instantiate these collections.
+ */
+
+- (NSMutableIndexSet *)deletedSectionIndexes {
+    if (_deletedSectionIndexes == nil) {
+        _deletedSectionIndexes = [[NSMutableIndexSet alloc] init];
+    }
+    return _deletedSectionIndexes;
+}
+
+- (NSMutableIndexSet *)insertedSectionIndexes {
+    if (_insertedSectionIndexes == nil) {
+        _insertedSectionIndexes = [[NSMutableIndexSet alloc] init];
+    }
+    return _insertedSectionIndexes;
+}
+
+- (NSMutableArray *)deletedRowIndexPaths {
+    if (_deletedRowIndexPaths == nil) {
+        _deletedRowIndexPaths = [[NSMutableArray alloc] init];
+    }
+    return _deletedRowIndexPaths;
+}
+
+- (NSMutableArray *)insertedRowIndexPaths {
+    if (_insertedRowIndexPaths == nil) {
+        _insertedRowIndexPaths = [[NSMutableArray alloc] init];
+    }
+    return _insertedRowIndexPaths;
+}
+
+- (NSMutableArray *)updatedRowIndexPaths {
+    if (_updatedRowIndexPaths == nil) {
+        _updatedRowIndexPaths = [[NSMutableArray alloc] init];
+    }
+    return _updatedRowIndexPaths;
 }
 
 @end
